a76c660d456d893cd111a6421755f861a355fcac
reubebe <38767286+reubebe@users.noreply.github.com>
2018-10-13 01:07:25 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -19,22 +19,22 @@ library(dplyr)
 ##############################################################################
 
 # read training data
-trainingSubjects <- read.table(file.path(dataPath, "train", "subject_train.txt"))
-trainingValues <- read.table(file.path(dataPath, "train", "X_train.txt"))
-trainingActivity <- read.table(file.path(dataPath, "train", "y_train.txt"))
+trainingSubjects <- read.table(file.path(Users/addison/Desktop/UCIHARDataset/train, "train", "subject_train.txt"))
+trainingValues <- read.table(file.path(Users/addison/Desktop/UCIHARDataset/train, "train", "X_train.txt"))
+trainingActivity <- read.table(file.path(Users/addison/Desktop/UCIHARDataset/train, "train", "y_train.txt"))
 
 # read test data
-testSubjects <- read.table(file.path(dataPath, "test", "subject_test.txt"))
-testValues <- read.table(file.path(dataPath, "test", "X_test.txt"))
-testActivity <- read.table(file.path(dataPath, "test", "y_test.txt"))
+testSubjects <- read.table(file.path(Users/addison/Desktop/UCIHARDataset/test, "test", "subject_test.txt"))
+testValues <- read.table(file.path(Users/addison/Desktop/UCIHARDataset/test, "test", "X_test.txt"))
+testActivity <- read.table(file.path(Users/addison/Desktop/UCIHARDataset/test, "test", "y_test.txt"))
 
 # read features, don't convert text labels to factors
-features <- read.table(file.path(dataPath, "features.txt"), as.is = TRUE)
+features <- read.table(file.path(Users/addison/Desktop/UCIHARDataset, "features.txt"), as.is = TRUE)
   ## note: feature names (in features[, 2]) are not unique
   ##       e.g. fBodyAcc-bandsEnergy()-1,8
 
 # read activity labels
-activities <- read.table(file.path(dataPath, "activity_labels.txt"))
+activities <- read.table(file.path(Users/addison/Desktop/UCIHARDataset, "activity_labels.txt"))
 colnames(activities) <- c("activityId", "activityLabel")
 
 
